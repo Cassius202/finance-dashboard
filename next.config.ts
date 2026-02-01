36467645c5c91dacf08450193045ebd879eb9cc6
Cassius Samuel <cassiusejekwu@gmail.com>
2026-02-01 17:20:37 +0100

    fixed next-config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  output: 'export',           // ← this is the key line
+  basePath: '/finance-dashboard', // ← IMPORTANT if repo is not at root (e.g. username.github.io/your-repo-name)
+  assetPrefix: '/finance-dashboard/', // same as basePath
+  images: {
+    unoptimized: true,        // required for GitHub Pages
+  },
   /* config options here */
 };
 
